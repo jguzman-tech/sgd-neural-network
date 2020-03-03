b1be7bbc3da820fd647fc8ffd7d240e0f7bbf1ae
ws342 <ws342@nau.edu>
2020-03-03 16:50:17 -0700

    second

--- main.py
+++ main.py
@@ -24,18 +24,40 @@ from gradient import Gradient, GradientDescent, MeanLogisticLoss
 
 def NNetOneSplit(X_mat, y_vec, max_epochs, step_size, n_hidden_units, is_subtrain):
     X_subtrain = np.array([[]])
+    n_col = X_mat.shape[1]
     # V_mat contains all of the small w values for each hidden unite, layer 1
     # w_vec contains all of the small w values for y_hat, layer 2
-    V_mat = stats.zscore(np.random.random_sample(X_subtrain.shape[1], n_hidden_units)) / 10
-    w_vec = stats.zscore(np.random.random_sample(X_subtrain.shape[1])) / 10
+    V_mat = stats.zscore(np.random.random_sample(n_col, n_hidden_units)) / 10
+    w_vec = stats.zscore(np.random.random_sample(n_col)) / 10
+    # create a weight  list and put two weight in this list
+    weight_list = list();
+    weight_list.append(V_mat);
+    weight_list.append(w_vec);
     for i in range(is_subtrain.shape[0]):
         if(is_subtrain[i] == True):
             X_train.append(np.copy(X_mat[i]))
     for epoch in range(max_epochs):
         for row in range(X_subtrain.shape[0]):
-            w_vec = Forward_Propagation(V_mat)
-            w_vec = Back_Propagation(X_subtrain[row], h_vec, V_mat)
-    return V_mat, w_vec
+            # forward propagation
+            observation = X_subtrain[row]
+            h_list = ForwardPropagation(observation, weight_list)
+    
+    return h_list
+    #return V_mat, w_vec
+
+# forward propagation function
+def ForwardPropagation(X, weight_list)
+    h_list = list()
+    h_list.append(X)
+    for i in range(len(weight_list))
+        a_vec = np.matmul(X, weight_list[i])
+        if(i == len(weight_list))
+            h_list.append(a_vec)
+        else
+            h_vec = 1/(1+exp(-a_vec))
+            h_list.append(h_vec)
+
+    return h_list
 
 def Parse(fname):
     all_rows = []
@@ -49,6 +71,7 @@ def Parse(fname):
         std = np.std(temp_ar[:, col])
         if(std == 0):
             print("col " + str(col) + " has an std of 0")
+        temp_ar[:, col] = stats.zscore(temp_ar[:, col])`
     return temp_ar
 
 if len(sys.argv) < 4:
@@ -60,8 +83,8 @@ seed must be an int
     print(help_str)
     exit(0)
 
-num_folds = int(sys.argv[1])
-max_neighbors = int(sys.argv[2])
+step_size = int(sys.argv[1])
+n_hidden_units = int(sys.argv[2])
 seed = int(sys.argv[3])
 np.random.seed(seed)
 temp_ar = Parse("spam.data")
@@ -70,3 +93,6 @@ X = temp_ar[:, 0:-1] # m x n
 X = X.astype(float)
 y = np.array([temp_ar[:, -1]]).T 
 y = y.astype(int)
+
+
+print(NNetOneSplit(

6fe1552aa091237f74d97b9ab45b3e461995c5fd
Joseph Guzman <josephfguzman@gmail.com>
2020-03-04 15:51:07 -0700

    still problem with back propagation

--- main.py
+++ main.py
@@ -20,22 +20,23 @@ warnings.simplefilter('error') # treat warnings as errors
 from matplotlib.pyplot import figure
 figure(num=None, figsize=(30, 8), dpi=80, facecolor='w', edgecolor='k')
 matplotlib.rc('font', size=24)
-from gradient import Gradient, GradientDescent, MeanLogisticLoss
 
 def NNetOneSplit(X_mat, y_vec, max_epochs, step_size, n_hidden_units, is_subtrain):
-    X_subtrain = np.array([[]])
-    X_validation = np.array([[]])
+    train_indices = np.where(is_subtrain == True)[0]
+    validation_indices = np.where(is_subtrain == False)[0]
+    X_subtrain = X_mat[train_indices]
+    y_subtrain = y_vec[train_indices]
+    X_validation = X_mat[validation_indices]
+    y_validation = y_vec[validation_indices]
+
     n_col = X_mat.shape[1]
     # V_mat contains all of the small w values for each hidden unite, layer 1
     # w_vec contains all of the small w values for y_hat, layer 2
-    V_mat = stats.zscore(np.random.random_sample(n_col, n_hidden_units)) / 10
-    w_vec = stats.zscore(np.random.random_sample(n_col)) / 10
-    
-    for i in range(is_subtrain.shape[0]):
-        if(is_subtrain[i] == "TRUE"):
-            X_subtrain.append(np.copy(X_mat[i]))
-        if(is_subtrain[i] == "FALSE"):
-            X_validation.append(np.copy(X_mat[i]))
+    V_mat = stats.zscore(np.random.random_sample((n_col, n_hidden_units)))
+    w_vec = stats.zscore(np.random.random_sample(n_col))
+
+    y_tilde = np.copy(y_vec)
+    y_tilde[y_tilde == 0] = -1
 
     # the loss value
     loss_value = list()
@@ -52,51 +53,47 @@ def NNetOneSplit(X_mat, y_vec, max_epochs, step_size, n_hidden_units, is_subtrai
             observation = X_subtrain[row]
             h_list = ForwardPropagation(observation, weight_list)
             # back propagation
-            grad_w_list = Back_Propagation(h_list, weight_list)
+            grad_w_list = Back_Propagation(h_list, weight_list, y_tilde[row])
             # update the theta for each weight
-            for i in range(len(weight_list))
+            for i in range(len(weight_list)):
                 weight_list[i] = weight_list[i] - step_size * grad_w_list[i]
             weight_list_X.append(weight_list)
 
         ## compute the logistic loss for subtraihttps://forum.handsontable.com/t/adding-dictionary-map-array-as-data/4199/3n and validaiton
         # get prediction for subtrain and validaiton
-        
-        
-
     #return h_list
-    #return V_mat, w_vec
+    return V_mat, w_vec
 
 # logistic loss function
-def LogisticLoss(pred, label)
-    value = log(1 + exp(-label * pred)
+def LogisticLoss(pred, label):
+    value = np.log(1 + np.exp(-label * pred))
     return value
 
 # forward propagation function
-def ForwardPropagation(X, weight_list)
+def ForwardPropagation(X, weight_list):
     h_list = list()
     h_list.append(X)# is it the correct way to append a numpy array into a list? or use np.copy(X)
-    for i in range(len(weight_list))
-        a_vec = np.matmul(X, weight_list[i])
-        if(i == len(weight_list))
+    for i in range(len(weight_list)):
+        a_vec = np.matmul(X[np.newaxis], weight_list[i])
+        if(i == len(weight_list)):
             h_list.append(a_vec)# is it the correct way to append a numpy array into a list?
-        else
-            h_vec = 1/(1+exp(-a_vec))
+        else:
+            h_vec = 1/(1+np.exp(-a_vec))
             h_list.append(h_vec)# is it the correct way to append a numpy array into a list?
 
     return h_list
 
 # back progration function
-def Back_Propagation(h_list, w_list):
+def Back_Propagation(h_list, w_list, y_tilde):
     grad_w_list = list()
-    for l in range(2, 1):
-        if(l == 2):
-            grad_a = -1 * y_tilde / (1 + exp(np.matmul(h_list[l])))
-        else:
-            grad_h = np.matmul(w_list[l].T, grad_a)
-            grad_a = grad_h * h_list[l] * (1 - h_list[l])
-        grad_w = np.matmul(grad_a[l], h_list[l - 1].T)
-        grad_w_list.append(grad_w)# is it the correct way to append a numpy array into a list?
+    grad_a = -1 * y_tilde / (1 + np.exp(y_tilde * h_list[2][0]))
+    grad_w_list.append(np.matmul(grad_a, h_list[1]))
 
+    grad_h = w_list[1][np.newaxis].T * grad_a
+    grad_a = grad_h * h_list[1] * (1 - h_list[1])
+    import pdb; pdb.set_trace()
+    grad_w_list.append(np.matmul(grad_a, h_list[0].T[np.newaxis]))
+    
     return grad_w_list
 
 def Parse(fname):
@@ -111,7 +108,7 @@ def Parse(fname):
         std = np.std(temp_ar[:, col])
         if(std == 0):
             print("col " + str(col) + " has an std of 0")
-        temp_ar[:, col] = stats.zscore(temp_ar[:, col])`
+        temp_ar[:, col] = stats.zscore(temp_ar[:, col])
     return temp_ar
 
 if len(sys.argv) < 4:
@@ -148,5 +145,4 @@ X_mat = X[np.where(is_train)[0]]
 y_vec = y[np.where(is_train)[0]]
 
 # NNetOneSplit(X_mat, y_vec, max_epochs, step_size, n_hidden_units, is_subtrain)
-# import pdb; pdb.set_trace()
 V_mat, w_vec = NNetOneSplit(X_mat, y_vec, 10, 0.1, 10, is_subtrain)

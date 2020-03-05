c9cb899b4a5cd7ae7ed75215f487f4063c2dfb61
ws342 <ws342@nau.edu>
2020-03-04 18:53:37 -0700

    few

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def NNetOneSplit(X_mat, y_vec, max_epochs, step_size, n_hidden_units, is_subtrai
     # V_mat contains all of the small w values for each hidden unite, layer 1
     # w_vec contains all of the small w values for y_hat, layer 2
     V_mat = stats.zscore(np.random.random_sample((n_col, n_hidden_units)))
-    w_vec = stats.zscore(np.random.random_sample(n_hidden_units))
+    w_vec = stats.zscore(np.random.random_sample((n_hidden_units, 1)))
 
     y_tilde = np.copy(y_vec)
     y_tilde[y_tilde == 0] = -1
@@ -53,11 +53,12 @@ def NNetOneSplit(X_mat, y_vec, max_epochs, step_size, n_hidden_units, is_subtrai
             observation = X_subtrain[row]
             h_list = ForwardPropagation(observation, weight_list)
             # back propagation
+            
             grad_w_list = Back_Propagation(h_list, weight_list, y_tilde[row])
-            print(len(grad_w_list))
+            
             # update the theta for each weight
             for i in range(len(weight_list)):
-                weight_list[i] = weight_list[i] - step_size * grad_w_list[i]
+                weight_list[i] = weight_list[i] - step_size * grad_w_list[ 1 - i]
             weight_list_X.append(weight_list)
         
         ## compute the logistic loss for subtraihttps://forum.handsontable.com/t/adding-dictionary-map-array-as-data/4199/3n and validaiton
@@ -92,11 +93,12 @@ def Back_Propagation(h_list, w_list, y_tilde):
         if(i == 2):
             grad_a = -1 * y_tilde / (1 + np.exp(y_tilde * h_list[i]))
         else:
-            import pdb; pdb.set_trace()
-            grad_h = np.matmul(w_list[i].T, grad_a)
+            grad_h = np.matmul(grad_a, w_list[i].T)
             grad_a = grad_h * h_list[i] * (1 - h_list[i])
-    grad_w_list.append(np.matmul(h_list[i - 1].T, grad_a))
-    
+        
+        
+        grad_w_list.append(np.matmul(h_list[i - 1][np.newaxis].T, grad_a))
+
     return grad_w_list
 
 def Parse(fname):
